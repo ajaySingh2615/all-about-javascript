0408efdc5385f7a6439842f3c893104fd0a4145a
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-11-15 02:06:45 +0530

    finished fundamental part - 1 of javascript

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -44,3 +44,46 @@ console.log("yearChanged=", typeof year);
 console.log("null typeOf=", typeof null);
 
 */
+
+///////////////////////////////////////////LET, VAR AND CONST/////////////////////////////////////////
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+
+// var job = "programmer";
+// job = "teacher";
+
+///////////////////////////////////////////STRINGS AND TEMPLATE LITERALS/////////////////////////////////////////
+
+// const firstName = "mike";
+// const job = "coder";
+// const birthYear = 2000;
+// const year = 2024;
+
+// const jonas = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+// console.log(jonas);
+
+///////////////////////////////////////////TYPE CONVERSION AND TYPE COERCION/////////////////////////////////////////
+
+// TYPE CONVERSION
+// const inputYear = "2000";
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
+// console.log(Number("mike"));
+// console.log(typeof NaN);
+// console.log(String(23));
+
+// TYPE COERCION
+// console.log("I am " + 23 + " years old!");
+// console.log("23" - "10" - 3);
+// console.log("23" + "10" + 3);
+// console.log("23" * "2");
+
+// LOGICAL OPERATORS
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
