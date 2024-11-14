94124ab64b493fe8f6884e51e7b11e47aca88c4a
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-11-14 22:48:30 +0530

    values and variable in js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+let js = "amazing";
+// if (js === "amazing") alert("Javascript is fun");
+
+console.log("mike");
+console.log(23);
+
+let firstName = "mike";
+console.log("firstName = ", firstName);
+
+///////////////////////////////// not to do////////////////////////////////////////////////////
+// let 3years = 10;
+// let jonas&ele = 10;
+// let new  = 26;
+
+/////////////////////////////////// these two are allowed/////////////////////////////////////
+// let _ismike = 10;
+// let $mike = 10;
+
+let myFirstJob = "coding";
+let myCurrentJob = "coding";
