3c49cdc6a30114dff02a753758f88545d10104ff
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-11-16 00:54:25 +0530

    done modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,10 +4,25 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closedModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-  });
+  btnsOpenModal[i].addEventListener("click", openModal);
+  btnCloseModal.addEventListener("click", closedModal);
+  overlay.addEventListener("click", closedModal);
 }
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closedModal();
+  }
+});
